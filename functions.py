6cf6c7173c6373c93bf173e1bc1f787bcb37ef45
AnaPaulaJuarez <51324495+AnaPaulaJuarez@users.noreply.github.com>
2023-02-07 16:17:56 -0600

    Update functions.py
    Creamos una funcion para la descarga de precios que nos regresa precios, precios mensuales y tickers

--- functions.py
+++ functions.py
@@ -27,7 +27,23 @@ from pypfopt import risk_models
 from pypfopt import expected_returns
 
 
+def descargaPrecios(fechaInicio : "Fecha de inicio del backtest", fechaFin : "Fecha de fin del backtest", 
+                    datos : "Posiciones del NAFTRAC"):
+    """
+    descargaPrecios es una función que devuelve los precios históricos en temporalidad diaria y mensual 
+    para los tickers que componen al índice NAFTRAC en la fecha ingresada.
+    """
+    tickersFiltro = ["KOFL", "KOFUBL", "USD", "MXN", "BSMXB", "NMKA"]
+    tickers = {datos.loc[i, "Ticker"].replace("*", "").replace(".", "-") + ".MX":
+               datos.loc[i, "Peso (%)"] / 100 for i in range(len(datos)) if datos.loc[i, "Ticker"] not in tickersFiltro}
 
+    precios = pd.DataFrame()
+    for ticker in tickers.keys():
+        precios[ticker] = yf.download(ticker, start=fechaInicio, end=fechaFin, progress=False)["Adj Close"]
+        
+    precios.dropna(axis=1, inplace=True)
+    preciosMensuales = precios[precios.index.to_series().diff().dt.days > 28]
+    return precios, preciosMensuales, tickers
 
     
 
