aaced4059c73477be177108a7351aa883cda15c0
valearz <51324698+valearz@users.noreply.github.com>
2023-02-12 13:21:47 -0600

    cambiamos algunos errores con ayuda del chat gpt ya que nos corria en datalore pero en visual studio nos aparecian bastantes correos nuevos, la computadora de mi compañera dejo de funcionar debido al almacenamiento pero lo realizamos juntas en la mia.

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import data, functions, visualization
+import pandas as pd
 
-
-def estrategiaInversionPasiva(capital : "Capital inicial", comision : "Comisión", 
-                              fechaInicio : "Fecha de inicio del backtest", fechaFin : "Fecha de fin del backtest"):
+def estrategiaInversionPasiva(capital: float, comision: float, fechaInicio: str, fechaFin: str):
     """
     estrategiaInversionPasiva es una función que elabora el testeo de la estrategia de inversión pasiva.
     
+    :param capital: Capital inicial.
+    :param comision: Comisión.
+    :param fechaInicio: Fecha de inicio del backtest.
+    :param fechaFin: Fecha de fin del backtest.
     """
     
     datos = data.lecturaDatos(fechaInicio)
-    precios, preciosMensuales, tickers = functions.descargaobtener_precios(fecha_inicio, fecha_fin, posiciones)
+    precios, preciosMensuales, tickers = functions.descargaobtener_precios(fechaInicio, fechaFin, tickers)
     pos_info_pasiva, cap_evol_pasiva, metricas_pasiva = functions.optimizar_estrategia_pasiva(precios, tickers, capital, comision)
     fig = visualization.visualEstrategia(cap_evol_pasiva)
     
     return pos_info_pasiva, cap_evol_pasiva, metricas_pasiva, fig
 
-def atribucionDesempeño(df_pasiva : "Estrategia Pasiva", df_activa : "Estrategia Activa", tasaLibreRiesgo : "Tasa libre riesgo"):
+def atribucionDesempeño(df_pasiva: pd.DataFrame, df_activa: pd.DataFrame, tasaLibreRiesgo: float):
     """
     atribucionDesempeño es una función que devuelve las medidas de atribución al desempeño de las estrategias de inversión pasiva
     y activa.
     
+    Parameters:
+    df_pasiva: pandas DataFrame
+        DataFrame con información de la estrategia pasiva.
+    df_activa: pandas DataFrame
+        DataFrame con información de la estrategia activa.
+    tasaLibreRiesgo: float
+        Tasa libre de riesgo para calcular la atribución al desempeño.
+        
+    Returns:
+    medidas: dict
+        Diccionario con las medidas de atribución al desempeño.
     """
     
-    medidas = functions.rendimientoAtribuido(estrategiaA, estrategiaB, tasaRiesgo)
+    medidas = functions.rendimientoAtribuido(df_pasiva, df_activa, tasaLibreRiesgo)
     
     return medidas
-
