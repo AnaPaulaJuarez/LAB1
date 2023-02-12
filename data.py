77237d296055ee390bc3e87d9da391abf769f9b9
valearz <51324698+valearz@users.noreply.github.com>
2023-02-12 12:38:29 -0600

    cambios en base a errores de la depuración

--- data.py
+++ data.py
@@ -1,12 +1,13 @@
+
 import pandas as pd
 import numpy as np
 
-def lecturaDatos(fecha : "Ingresar la fecha para la lectura del archivo de Excel que contiene las posiciones del NAFTRAC (yyyy-mm-dd)"):
+def lecturaDatos(fecha: str) :
     """
     lecturaDatos es una función que lee el archivo de Excel que contiene las posiciones del NAFTRAC para la fecha deseada.
     
     """
     
-    datos = pd.read_csv("files/NAFTRAC_" + fecha.replace("-", "") + ".csv", skiprows = 2).dropna()
+    datos = pd.read_csv("files/NAFTRAC_" + fecha.replace("-", "") + ".csv", skiprows=2).dropna()
     
     return datos
--- functions.py
+++ functions.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # -- Sheet --
-
-
 """
 # -- --------------------------------------------------------------------------------------------------- -- #
 # -- project: Laboratorio 1. Inversión de Capital                                                        -- #
@@ -12,11 +10,12 @@
 # -- repository:                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
-
 import pandas as pd
 pd.set_option('display.float_format', lambda x: '%.4f' % x)
 import yfinance as yf
 import numpy as np
+import pypfopt as pf
+
 from pypfopt import EfficientFrontier
 from pypfopt import risk_models
 from pypfopt import expected_returns
@@ -60,93 +59,4 @@ def optimizar_estrategia_pasiva(precios, tickers, capital, comision):
     metricas_pasiva = pd.DataFrame({"Capital Inicial": capital, "Capital Invertido": cap_inv, "Efectivo": capital - cap_inv, "Capital Final": capital - cap_inv + cap_evol_pasiva["Evo Capital"].iloc[-1], "Rend. Efectivo %": cap_evol_pasiva["Rend. Mensual Acum."].iloc[-1] * 100}, index=["Estrategia Pasiva"])  
     
     return pos_info_pasiva, cap_evol_pasiva, metricas_pasiva
-
-def inversionActivaEficiente(datosDivisionFecha: "Fecha de partición de datos", 
-                    diarioPrecios: "Precios diarios", preciosMensuales: "Precios mensuales", 
-                    fondos: "Capital", tarifaComision: "Comisión"):
-    
-    # Portafolio eficiente (Sharpe)
-    preciosDivisados = diarioPrecios[diarioPrecios.index <= datosDivisionFecha]
-    rendimientoEsperado = expected_returns.mean_historical_return(preciosDivisados, compounding = False, log_returns = True)
-    covarianza = risk_models.sample_cov(preciosDivisados, log_returns = True)
-    portafolioEficiente = EfficientFrontier(rendimientoEsperado, covarianza)
-    pesos = portafolioEficiente.max_sharpe()
-    pesosDataFrame = pd.DataFrame(pesos, columns = list(pesos.keys()), index = ["Ponderaciones"])
-    pesosDataFrame = pesosDataFrame[pesosDataFrame > 0].dropna(axis = 1)
-    
-    # Posiciones iniciales
-    informacionInversionActiva = pd.DataFrame(index = pesosDataFrame.columns, columns = ["Títulos", "Costo de Compra Bruto", "Comisión",
-                                                                      "Costo de Compra Total", "Ponderación"])
-    preciosDespuesFecha = diarioPrecios.loc[diarioPrecios.index > datosDivisionFecha, list(pesosDataFrame.keys())]
-    preciosMensualesDespuesFecha = preciosMensuales.loc[preciosMensuales.index > datosDivisionFecha, list(pesosDataFrame.keys())]
-    preciosMensualesDataFrame = pd.DataFrame()
-    preciosMensualesDataFrame.loc[preciosDespuesFecha.index[0], preciosMensualesDespuesFecha.columns] = preciosDespuesFecha.iloc[0, :]
-
-    for i in range(len(preciosMensualesDespuesFecha)):
-        preciosMensualesDataFrame.loc[preciosMensualesDespuesFecha.index[i], preciosMensualesDespuesFecha.columns] = preciosMensualesDespuesFecha.iloc[i, :]
-
-    for ticker in pesosDataFrame.columns:
-        informacionInversionActiva.loc[ticker, "Títulos"] = np.floor((fondos * pesosDataFrame.loc["Ponderaciones", ticker]) / 
-                                                                    (preciosMensualesDataFrame.loc[preciosMensualesDataFrame.index[0], ticker] * (1 + tarifaComision)))
-        informacionInversionActiva.loc[ticker, "Costo de Compra Bruto"] = informacionInversionActiva.loc[ticker, "Títulos"] * preciosMensualesDataFrame.loc[preciosMensualesDataFrame.index[0], ticker]
-        informacionInversionActiva.loc[ticker, "Comisión"] = informacionInversionActiva.loc[ticker, "Títulos"] * preciosMensualesDataFrame.loc[preciosMensualesDataFrame.index[0], ticker] * tarifaComision
-        informacionInversionActiva.loc[ticker, "Costo de Compra Total"] = informacionInversionActiva.loc[ticker, "Títulos"] * preciosMensualesDataFrame.loc[preciosMensualesDataFrame.index[0], ticker] * (1 + tarifaComision)
-        informacionInversionActiva.loc[ticker, "Ponderación"] = (informacionInversionActiva.loc[ticker, "Títulos"] * preciosMensualesDataFrame.loc[preciosMensualesDataFrame.index[0], ticker]) / fondos
-
-    return informacionInversionActiva
-
-def backtest(datosDivisionFecha, diarioPrecios, preciosMensuales, fondos, tarifaComision, benchmark_returns):
-    # Ejecutar la función inversionActivaEficiente
-    informacionInversionActiva = inversionActivaEficiente(datosDivisionFecha, diarioPrecios, preciosMensuales, fondos, tarifaComision)
-    
-    # Calcular rendimientos del portafolio y del benchmark
-    preciosMensualesDespuesFecha = preciosMensuales.loc[preciosMensuales.index > datosDivisionFecha, informacionInversionActiva.index]
-    rendimientosPortafolio = (preciosMensualesDespuesFecha / preciosMensualesDespuesFecha.iloc[0, :]).prod() - 1
-    rendimientosBenchmark = (benchmark_returns.loc[preciosMensualesDespuesFecha.index] / benchmark_returns.loc[preciosMensualesDespuesFecha.index[0]]).prod() - 1
-    
-    # Calcular la tasa de rendimiento esperada y la volatilidad del portafolio
-    tasaRendimientoEsperada = rendimientosPortafolio.mean()
-    volatilidad = rendimientosPortafolio.std()
-    
-    # Calcular la tasa de rendimiento esperada y la volatilidad del benchmark
-    tasaRendimientoEsperadaBenchmark = rendimientosBenchmark.mean()
-    volatilidadBenchmark = rendimientosBenchmark.std()
-    
-    # Calcular el Sharpe ratio del portafolio y del benchmark
-    sharpeRatio = (tasaRendimientoEsperada - 0.03) / volatilidad
-    sharpeRatioBenchmark = (tasaRendimientoEsperadaBenchmark - 0.03) / volatilidadBenchmark
-
-    # Graficar los rendimientos del portafolio y del benchmark
-    plt.plot(rendimientosPortafolio.index, rendimientosPortafolio, label="Portafolio")
-    plt.plot(rendimientosBenchmark.index, rendimientosBenchmark, label="Benchmark")
-    plt.xlabel("Fecha")
-    plt.ylabel("Rendimiento")
-    plt.legend()
-    plt.show()
-
-def rendimientoAtribuido(estrategiaA : pd.DataFrame, estrategiaB : pd.DataFrame, tasaRiesgo : float):
-
-    resultados = pd.DataFrame(index = ["prom_mensual", "acum_mensual", "ratio_sharpe"], columns = ["Descripción", "Estrategia A", "Estrategia B"]) #Creamos un nuevo DataFrame "RESULTADOS"
-    resultados.loc["prom_mensual", "Descripción"] = "Rend Prom mensual" #Asignamos valores a las celdas
-    resultados.loc["acum_mensual", "Descripción"] = "Rend mensual acum" #Asignamos valores a las celdas
-    resultados.loc["ratio_sharpe", "Descripción"] = "Ratio Sharpe" #Asignamos valores a las celdas
-
-    #Estrategia A 
-    resultados.loc["prom_mensual", "Estrategia A"] = estrategiaA["Rend Mensual"].dropna().mean() #Calculamos el rend promedio mensual y asignamos los valores a las celdas correspondientes en el DataFrame
-    resultados.loc["acum_mensual", "Estrategia A"] = estrategiaA["Rend Mensual Acum"][-1] #Calculamos el rend promedio acumulado y asignamos los valores a las celdas correspondientes en el DataFrame
-    resultados.loc["ratio_sharpe", "Estrategia A"] = (estrategiaA["Rend Mensual"].dropna().mean() - tasaRiesgo) / estrategiaA["Rend Mensual"].dropna().std() #Calculamos el ratio sharpe y asignamos los valores a las celdas correspondientes en el DataFrame
-   
-    #Estrategia B
-    resultados.loc["prom_mensual", "Estrategia B"] = estrategiaB["Rend Mensual"].dropna().mean()
-    resultados.loc["acum_mensual", "Estrategia B"] = estrategiaB["Rend Mensual Acum"][-1]
-    resultados.loc["ratio_sharpe", "Estrategia B"] = (estrategiaB["Rend Mensual"].dropna().mean() - tasaRiesgo) / estrategiaB["Rend Mensual"].dropna().std() 
-
-    return resultados
-
-
-
-
-
-
-
 
